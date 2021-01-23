7c7ee8da6928fad132f208e5e6556177ef2eb431
cabrerojas <gcabrerojas@gmail.com>
2021-01-23 18:50:00 -0300

    Create 05-desestructuracion-basico.ts

---
+++ src/ejercicios/05-desestructuracion-basico.ts
@@ -0,0 +1,50 @@
+
+/*
+    ===== Código de TypeScript =====
+
+    https://www.typescriptlang.org/docs/handbook/basic-types.html
+*/
+
+interface Reproducto {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles;
+
+}
+
+interface Detalles{
+    autor: string;
+    anio: number;
+}
+
+const reproducto: Reproducto = {
+    volumen: 90,
+    segundo: 35,
+    cancion: "Mr. Brightside",
+    detalles: {
+        anio: 2020,
+        autor: "The Killers"
+    }
+}
+
+const autor = 'Fulano';
+
+const { volumen: vol, segundo, cancion, detalles } = reproducto;
+
+const { autor: autorDetalle } = detalles;
+
+
+
+// console.log('El volumen actual de:', vol);
+// console.log('El segundo actual de:', segundo );
+// console.log('La cancion actual de:', cancion);
+// console.log('El autor es:', autorDetalle);
+
+const dbz: string[] = ['Goku','Vegeta','Trunks'];
+const [ p1, ,p3 ] = dbz;
+
+console.log('Personaje 1:', p1);
+console.log('Personaje 1:', dbz[1]);
+console.log('Personaje 1:', p3);
+
